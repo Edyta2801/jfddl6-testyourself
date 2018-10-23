aceb1ed62c1f522871575cf792c0ced39fb55405
Karol szykula <karol.szykula@gmail.com>
2018-10-23 16:58:22 +0200

    wip

---
+++ script/script.js
@@ -0,0 +1,23 @@
+const menuToggle = document.querySelector('.nav-top__toggle');
+const menuList = document.querySelector('.section-nav-top__flex-wrapper');
+const menuListItems = document.querySelectorAll('.section-nav-top__flex-wrapper__menu-item');
+
+const menuListHeight = menuListItems[0].clientHeight * menuListItems.length;
+//console.log(menuListHeight);
+menuList.classList.add('nav-top__flex-wrapper--collapsed');
+
+menuToggle.addEventListener(
+    'click',
+    (event) => {
+        event.preventDefault();
+
+        if(menuList.classList.contains('nav-top__flex-wrapper--collapsed')){
+            menuList.classList.remove('nav-top__flex-wrapper--collapsed');
+            menuList.style.maxHeight = menuListHeight + 'px';
+        }else{
+            menuList.classList.add('nav-top__flex-wrapper--collapsed');
+        }
+
+
+    }
+)
