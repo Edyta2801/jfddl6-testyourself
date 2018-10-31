a5966de88b43dffbea9d4d3e85890401fd1a47b5
Karol szykula <karol.szykula@gmail.com>
2018-10-31 16:55:00 +0100

    wip

--- script/scroll-spy.js
+++ script/scroll-spy.js
@@ -1,60 +1,78 @@
-inView("#section-hero")
-  .on("enter", function() {
-    document
-      .querySelector("#section-nav-top__home-id")
-      .classList.add("active");
-      console.log('jest w hero')
-  })
-  .on("exit", function() {
-    document
-      .querySelector("#section-nav-top__home-id")
-      .classList.remove("active");
-  });
+homeNavId = document.querySelector('#section-nav-top__home-id');
+functionNavId = document.querySelector('#section-nav-top__function-id');
+productNavId = document.querySelector('#section-nav-top__product-id');
+teamNavId = document.querySelector('#section-nav-top__team-id');
+premiereNavId = document.querySelector('#section-nav-top__premiere-reg-id');
 
-inView("#section-function")
-  .on("enter", function() {
-    document
-      .querySelector("#section-nav-top__function-id")
-      .classList.add("active");
-  })
-  .on("exit", function() {
-    document
-      .querySelector("#section-nav-top__function-id")
-      .classList.remove("active");
-  });
+function NavItem(itemId, sectionId) {
+    this.id = itemId;
+    this.sectionId = sectionId;
+}
 
-inView("#section-info-products")
-  .on("enter", function() {
-    document
-      .querySelector("#section-nav-top__product-id")
-      .classList.add("active");
-  })
-  .on("exit", function() {
-    document
-      .querySelector("#section-nav-top__product-id")
-      .classList.remove("active");
-  });
 
-inView("#section-info-team")
-  .on("enter", function() {
-    document
-      .querySelector("#section-nav-top__team-id")
-      .classList.add("active");
-  })
-  .on("exit", function() {
-    document
-      .querySelector("#section-nav-top__team-id")
-      .classList.remove("active");
-  });
 
-inView("#section-registration-premiere")
-  .on("enter", function() {
-    document
-      .querySelector("#section-nav-top__premiere-reg-id")
-      .classList.add("active");
-  })
-  .on("exit", function() {
-    document
-      .querySelector("#section-nav-top__premiere-reg-id")
-      .classList.remove("active");
-  });
+
+
+
+
+
+// inView("#section-hero")
+//   .on("enter", function() {
+//     document
+//       .querySelector("#section-nav-top__home-id")
+//       .classList.add("active");
+//       console.log('jest w hero')
+//   })
+//   .on("exit", function() {
+//     document
+//       .querySelector("#section-nav-top__home-id")
+//       .classList.remove("active");
+//   });
+
+// inView("#section-function")
+//   .on("enter", function() {
+//     document
+//       .querySelector("#section-nav-top__function-id")
+//       .classList.add("active");
+//   })
+//   .on("exit", function() {
+//     document
+//       .querySelector("#section-nav-top__function-id")
+//       .classList.remove("active");
+//   });
+
+// inView("#section-info-products")
+//   .on("enter", function() {
+//     document
+//       .querySelector("#section-nav-top__product-id")
+//       .classList.add("active");
+//   })
+//   .on("exit", function() {
+//     document
+//       .querySelector("#section-nav-top__product-id")
+//       .classList.remove("active");
+//   });
+
+// inView("#section-info-team")
+//   .on("enter", function() {
+//     document
+//       .querySelector("#section-nav-top__team-id")
+//       .classList.add("active");
+//   })
+//   .on("exit", function() {
+//     document
+//       .querySelector("#section-nav-top__team-id")
+//       .classList.remove("active");
+//   });
+
+// inView("#section-registration-premiere")
+//   .on("enter", function() {
+//     document
+//       .querySelector("#section-nav-top__premiere-reg-id")
+//       .classList.add("active");
+//   })
+//   .on("exit", function() {
+//     document
+//       .querySelector("#section-nav-top__premiere-reg-id")
+//       .classList.remove("active");
+//   });
