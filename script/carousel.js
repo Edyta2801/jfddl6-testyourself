2ff51b6b383869861f7298fe8c092205b0939d2c
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-11-01 22:54:13 +0100

    carousel wip

--- script/carousel.js
+++ script/carousel.js
@@ -1,21 +1,68 @@
 
-let index=0;
+let index = 0;
+let autoCarousel = true;
+const squareIndicator = document.body.querySelectorAll('.section-hero__slide-indicator');
+const imageBox = document.body.querySelectorAll('.section-hero__background');
 
 carousel();
+eventListener();
 
-function carousel() {
-let i;
-const imageBox = document.body.querySelectorAll('.section-hero__background');
-const squareIndicator=document.body.querySelectorAll('.section-hero__slide-indicator');
-for (i = 0; i < imageBox.length; i++) {
-  imageBox[i].style.display = "none";
-} 
- index++;
-if(index>imageBox.length){index=1}
-imageBox[index-1].style.display="initial";
-
-setTimeout(carousel,5000);
+function hideBackgroundImages() {
+  for (let i = 0; i < imageBox.length; i++) {
+    imageBox[i].style.display = "none";
+  }
 }
+function clearSquareIndicators() {
+  for (let i = 0; i < imageBox.length; i++) {
+    squareIndicator[i].style.backgroundColor = "grey";
+  }
+}
+function carousel() {
+  if (autoCarousel == true) {
+    hideBackgroundImages();
+    clearSquareIndicators();
+    index++;
+    if (index > imageBox.length) { index = 1 }
+    imageBox[index - 1].style.display = "initial";
+    squareIndicator[index - 1].style.backgroundColor = "black";
+    setTimeout(carousel, 5000);
+  } else {
+
+  }
+
+
+}
+
+const getObject = (event) => event.target;
+
+function getFocus() { getObject(event).style.border = "2px solid white"; }
+
+function outOfFocus() {
+  for (i = 0; i < squareIndicator.length; i++) {
+    squareIndicator[i].style.border = "none";
+  }
+}
+function goToSlide() {
+  autoCarousel = false;
+  clearSquareIndicators();
+  let obj =getObject(event)
+  obj.style.backgroundColor = "black";
+  
+  
+  
+  
+}
+
+
+
+function eventListener() {
+  for (let i = 0; i < imageBox.length; i++) {
+    squareIndicator[i].addEventListener("mouseover", getFocus);
+    squareIndicator[i].addEventListener("mouseout", outOfFocus);
+    squareIndicator[i].addEventListener("click", goToSlide)
+  }
+}
+
 
 
 
