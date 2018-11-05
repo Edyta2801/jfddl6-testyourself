f62eaf142dfc026e5bce723065d0cbac1b5749ce
Karol-Szykula <karol.szykula@wp.pl>
2018-11-05 21:39:52 +0100

    Create game skeleton

---
+++ script/game.js
@@ -0,0 +1,57 @@
+class Card {
+    constructor() {
+        this.visible = false
+        this.completed = false
+        // this.image = jakiś adres do pliku z obrazkiem
+        //this.Id = null przypisanie id chyba dopiero w funkcji generate array w obiekcie Game
+    }
+
+    isVisible() {
+        return this.visible
+    }
+
+    isCompleted() {
+        return this.completed
+    }
+}
+
+class Game {
+    constructor() {
+        // this.arrayOfCards = [] //array w której będą obiekty typu Card
+        //this.preDefinedArraySizes
+        //this.moveCounter
+        //this.timer
+    }
+
+    init() {
+
+    }
+
+    startGame() {
+
+    }
+
+    render() {
+
+    }
+
+    clickCard() {
+
+    }
+
+    generateArrayOfCards() {
+        // miało być generateBoard, ale po przemyśleniu wydaje mi się, że jednak lepiej po prostu array, jak coś to można zmienić
+    }
+
+    shuffleDecksInArray() {
+
+    }
+
+    chooseLevel() {
+
+    }
+
+    // resetGame() {
+    // robimy taką funkcję ?
+    // }
+}
