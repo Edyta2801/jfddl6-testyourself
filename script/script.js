139bda3a22c69ac393ee2198e106db75158be909
Karol-Szykula <karol.szykula@wp.pl>
2018-11-05 19:59:45 +0100

    Add feature of closing menu after it is being clicked

--- script/script.js
+++ script/script.js
@@ -1,23 +1,37 @@
-const menuToggle = document.querySelector(".section-nav-top__toggle");
-const menuList = document.querySelector(".section-nav-top__flex-wrapper");
-const menuListItems = document.querySelectorAll(
-  ".section-nav-top__item"
-);
+(function () {
+  'use strict'
+  const menuToggle = document.querySelector(".section-nav-top__toggle");
+  const menuList = document.querySelector(".section-nav-top__flex-wrapper");
+  const menuListItems = document.querySelectorAll(
+    ".section-nav-top__item"
+  );
 
-const menuListHeight = menuListItems[0].clientHeight * menuListItems.length;
+  const menuListHeight = menuListItems[0].clientHeight * menuListItems.length;
 
-menuList.classList.add("section-nav-top__flex-wrapper--collapsed");
+  menuList.classList.add("section-nav-top__flex-wrapper--collapsed");
 
-menuToggle.addEventListener("click", event => {
-  event.preventDefault();
+  menuToggle.addEventListener("click", event => {
+    event.preventDefault();
 
-  if (menuList.classList.contains("section-nav-top__flex-wrapper--collapsed")) {
-    menuList.classList.remove("section-nav-top__flex-wrapper--collapsed");
-    menuList.style.maxHeight = menuListHeight + "px";
-  } else {
-    menuList.classList.add("section-nav-top__flex-wrapper--collapsed");
-  }
-});
+    if (menuList.classList.contains("section-nav-top__flex-wrapper--collapsed")) {
+      menuList.classList.remove("section-nav-top__flex-wrapper--collapsed");
+      menuList.style.maxHeight = menuListHeight + "px";
+    } else {
+      menuList.classList.add("section-nav-top__flex-wrapper--collapsed");
+    }
+  });
 
+  const linkNames = ['home-id', 'function-id', 'product-id', 'team-id', 'premiere-reg-id']
 
+  const navLinks = linkNames.map((element) => {
+    return document.querySelector(`#section-nav-top__${element}`);
+  });
 
+  navLinks.forEach((element) => {
+
+    element.addEventListener('click', event => {
+
+      menuList.classList.add("section-nav-top__flex-wrapper--collapsed");
+    })
+  })
+})()
--- script/scroll-spy.js
+++ script/scroll-spy.js
@@ -1,60 +1,61 @@
+(function () {
+    'use strict'
+    const linkNames = ['home-id', 'function-id', 'product-id', 'team-id', 'premiere-reg-id'];
+    const sectionNames = ['hero', 'function', 'info-products', 'info-team', 'registration-premiere'];
 
-'use strict'
-const linkNames = ['home-id', 'function-id', 'product-id', 'team-id', 'premiere-reg-id'];
-const sectionNames = ['hero', 'function', 'info-products', 'info-team', 'registration-premiere'];
+    const navLinks = linkNames.map((element) => {
+        return document.querySelector(`#section-nav-top__${element}`);
+    });
 
-const navLinks = linkNames.map((element) => {
-    return document.querySelector(`#section-nav-top__${element}`);
-});
+    const navSections = sectionNames.map((element) => {
+        return document.querySelector(`#section-${element}`);
+    });
 
-const navSections = sectionNames.map((element) => {
-    return document.querySelector(`#section-${element}`);
-});
+    const navbarHeight = document.querySelector('.section-nav-top').clientHeight;
 
-const navbarHeight = document.querySelector('.section-nav-top').clientHeight;
-
-function NavItem(itemEl, sectionEl) {
-    this.itemEl = itemEl;
-    this.sectionEl = sectionEl;
-    this.elementHeight = this.sectionEl.clientHeight;
-}
-
-NavItem.prototype.setActive = function () {
-    this.itemEl.classList.add('active');
-}
-NavItem.prototype.setInActive = function () {
-    this.itemEl.classList.remove('active');
-}
-
-let navItems = navLinks.map((element, i) => {
-    return new NavItem(element, navSections[i]);
-});
-
-document.addEventListener('scroll', () => {
-    if (Math.ceil(window.scrollY + window.innerHeight) === document.body.clientHeight) {
-        navItems.forEach((element) => {
-            element.setInActive();
-        })
-        navItems[4].setActive();
-    } else if (window.scrollY + 8 * navbarHeight > navItems[3].sectionEl.offsetTop) {
-        navItems.forEach((element) => {
-            element.setInActive();
-        })
-        navItems[3].setActive();
-    } else if (window.scrollY + 2.5 * navbarHeight > navItems[2].sectionEl.offsetTop) {
-        navItems.forEach((element) => {
-            element.setInActive();
-        })
-        navItems[2].setActive();
-    } else if (window.scrollY + 4 * navbarHeight > navItems[1].sectionEl.offsetTop) {
-        navItems.forEach((element) => {
-            element.setInActive();
-        })
-        navItems[1].setActive();
-    } else {
-        navItems.forEach((element) => {
-            element.setInActive();
-        })
-        navItems[0].setActive();
+    function NavItem(itemEl, sectionEl) {
+        this.itemEl = itemEl;
+        this.sectionEl = sectionEl;
+        this.elementHeight = this.sectionEl.clientHeight;
     }
-});
+
+    NavItem.prototype.setActive = function () {
+        this.itemEl.classList.add('active');
+    }
+    NavItem.prototype.setInActive = function () {
+        this.itemEl.classList.remove('active');
+    }
+
+    let navItems = navLinks.map((element, i) => {
+        return new NavItem(element, navSections[i]);
+    });
+
+    document.addEventListener('scroll', () => {
+        if (Math.ceil(window.scrollY + window.innerHeight) === document.body.clientHeight) {
+            navItems.forEach((element) => {
+                element.setInActive();
+            })
+            navItems[4].setActive();
+        } else if (window.scrollY + 8 * navbarHeight > navItems[3].sectionEl.offsetTop) {
+            navItems.forEach((element) => {
+                element.setInActive();
+            })
+            navItems[3].setActive();
+        } else if (window.scrollY + 2.5 * navbarHeight > navItems[2].sectionEl.offsetTop) {
+            navItems.forEach((element) => {
+                element.setInActive();
+            })
+            navItems[2].setActive();
+        } else if (window.scrollY + 4 * navbarHeight > navItems[1].sectionEl.offsetTop) {
+            navItems.forEach((element) => {
+                element.setInActive();
+            })
+            navItems[1].setActive();
+        } else {
+            navItems.forEach((element) => {
+                element.setInActive();
+            })
+            navItems[0].setActive();
+        }
+    });
+})()
