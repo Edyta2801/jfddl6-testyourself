531c37586a7507ff076c5fea62a50151af9b3ca7
Karol szykula <karol.szykula@gmail.com>
2018-11-07 16:00:30 +0100

    Add generate array function

--- script/game.js
+++ script/game.js
@@ -1,9 +1,9 @@
 class Card {
-    constructor(text) {
+    constructor(id) {
         this.visible = true
         this.completed = false
         this.image = null //jakiś adres do pliku z obrazkiem
-        this.id = id //przypisanie id chyba dopiero w funkcji generate array w obiekcie Game
+        this.id = null || id //przypisanie id chyba dopiero w funkcji generate array w obiekcie Game
     }
 
     toggleVisible() {
@@ -29,17 +29,18 @@ class Card {
 
 class Game {
     constructor() {
-        this.arrayOfCards = [new Card(1), new Card(2), new Card(3), new Card(4)] //array w której będą obiekty typu Card
+        this.arrayOfCards = []//[new Card(1), new Card(2), new Card(3), new Card(4)] //array w której będą obiekty typu Card
         //this.preDefinedArraySizes
-        this.boardDimension = 4//2,4,6,8,10 max 2x2,4x4...
+        this.boardDimension = 4// 2,4,6,8,10 max 2x2,4x4...
         this.moveCounter = 0
         this.timer = null
         this.cardId = 1 //card/cell id for the gameboard cells/cards
 
+        this.init()
     }
 
     init() {
-
+        this.generateArrayOfCards()
     }
 
     startGame() {
@@ -47,44 +48,46 @@ class Game {
     }
 
     render() {
-        //create gameboard container
-        document.body.innerHTML = ""
-        const gameBoard = document.createElement('div')
-        gameBoard.classList.add('gameboard')
-        document.body.appendChild(gameBoard)
-        //create board depending on board dimension/level
-        //rows (as many as dimension)
-        let cardIndex = 0;
-      
-        while (cardIndex < this.arrayOfCards.length) {
-            for (let r = 0; r < this.boardDimension; r++) {
-                let row = document.createElement('div')
-                row.classList.add('boardRow')
-                gameBoard.appendChild(row)
-                //single cards(as many as dimension) in rows
-                for (let i = 0; i < this.boardDimension; i++) {
-                    let singleCard = document.createElement('div')
-                //is card completed?
-                    if (this.arrayOfCards[cardIndex].completed==true) {
-                        if(singleCard.classList.contains('card--visible')){
-                            singleCard.classList.remove('card--visible')}
-                        singleCard.classList.add('card--completed')
-                            //is card visible?
-                    }else if (this.arrayOfCards[cardIndex].visible==true) {
-                        if(singleCard.classList.contains('card--covered')){
-                            singleCard.classList.remove('card--covered')}
-                        /* singleCard.classList.remove('card--completed')//testing only*/
-                        singleCard.classList.add('card--visible')
-                            //set card covered
-                    }else singleCard.classList.add('card--covered')
-                    
-                    singleCard.setAttribute('id', this.cardId)
-                    row.appendChild(singleCard)
-                    this.cardId++
-                    cardIndex++
-                }
-            }
-        }
+        // //create gameboard container
+        // document.body.innerHTML = ""
+        // const gameBoard = document.createElement('div')
+        // gameBoard.classList.add('gameboard')
+        // document.body.appendChild(gameBoard)
+        // //create board depending on board dimension/level
+        // //rows (as many as dimension)
+        // let cardIndex = 0;
+
+        // while (cardIndex < this.arrayOfCards.length) {
+        //     for (let r = 0; r < this.boardDimension; r++) {
+        //         let row = document.createElement('div')
+        //         row.classList.add('boardRow')
+        //         gameBoard.appendChild(row)
+        //         //single cards(as many as dimension) in rows
+        //         for (let i = 0; i < this.boardDimension; i++) {
+        //             let singleCard = document.createElement('div')
+        //             //is card completed?
+        //             if (this.arrayOfCards[cardIndex].completed == true) {
+        //                 if (singleCard.classList.contains('card--visible')) {
+        //                     singleCard.classList.remove('card--visible')
+        //                 }
+        //                 singleCard.classList.add('card--completed')
+        //                 //is card visible?
+        //             } else if (this.arrayOfCards[cardIndex].visible == true) {
+        //                 if (singleCard.classList.contains('card--covered')) {
+        //                     singleCard.classList.remove('card--covered')
+        //                 }
+        //                 /* singleCard.classList.remove('card--completed')//testing only*/
+        //                 singleCard.classList.add('card--visible')
+        //                 //set card covered
+        //             } else singleCard.classList.add('card--covered')
+
+        //             singleCard.setAttribute('id', this.cardId)
+        //             row.appendChild(singleCard)
+        //             this.cardId++
+        //             cardIndex++
+        //         }
+        //     }
+        // }
     }
 
 
@@ -96,7 +99,21 @@ class Game {
     }
 
     generateArrayOfCards() {
-        // miało być generateBoard, ale po przemyśleniu wydaje mi się, że jednak lepiej po prostu array, jak coś to można zmienić
+        let fullDim = (this.boardDimension * this.boardDimension) / 2
+
+        for (let i = 0; i < fullDim; i++) {
+            this.arrayOfCards[i] = new Card()
+            // tutaj linia dodająca url do każdej karty
+        }
+
+        const tempArr = this.arrayOfCards.map(element => Object.assign(Object.create(Card.prototype), element));
+
+
+        this.arrayOfCards = this.arrayOfCards.concat(tempArr)
+
+        this.arrayOfCards.forEach((element, i) => {
+            element.setId(i+1)
+        })
     }
 
     shuffleDecksInArray() {
