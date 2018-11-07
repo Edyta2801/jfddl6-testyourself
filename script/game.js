73e1f6158b80515b91c3709123f092cb2c7a77df
Karol szykula <karol.szykula@gmail.com>
2018-11-07 16:01:56 +0100

    Uncomment render function

--- script/game.js
+++ script/game.js
@@ -29,7 +29,7 @@ class Card {
 
 class Game {
     constructor() {
-        this.arrayOfCards = []//[new Card(1), new Card(2), new Card(3), new Card(4)] //array w której będą obiekty typu Card
+        this.arrayOfCards = [] //[new Card(1), new Card(2), new Card(3), new Card(4)] //array w której będą obiekty typu Card
         //this.preDefinedArraySizes
         this.boardDimension = 4// 2,4,6,8,10 max 2x2,4x4...
         this.moveCounter = 0
@@ -48,46 +48,46 @@ class Game {
     }
 
     render() {
-        // //create gameboard container
-        // document.body.innerHTML = ""
-        // const gameBoard = document.createElement('div')
-        // gameBoard.classList.add('gameboard')
-        // document.body.appendChild(gameBoard)
-        // //create board depending on board dimension/level
-        // //rows (as many as dimension)
-        // let cardIndex = 0;
+        //create gameboard container
+        document.body.innerHTML = ""
+        const gameBoard = document.createElement('div')
+        gameBoard.classList.add('gameboard')
+        document.body.appendChild(gameBoard)
+        //create board depending on board dimension/level
+        //rows (as many as dimension)
+        let cardIndex = 0;
 
-        // while (cardIndex < this.arrayOfCards.length) {
-        //     for (let r = 0; r < this.boardDimension; r++) {
-        //         let row = document.createElement('div')
-        //         row.classList.add('boardRow')
-        //         gameBoard.appendChild(row)
-        //         //single cards(as many as dimension) in rows
-        //         for (let i = 0; i < this.boardDimension; i++) {
-        //             let singleCard = document.createElement('div')
-        //             //is card completed?
-        //             if (this.arrayOfCards[cardIndex].completed == true) {
-        //                 if (singleCard.classList.contains('card--visible')) {
-        //                     singleCard.classList.remove('card--visible')
-        //                 }
-        //                 singleCard.classList.add('card--completed')
-        //                 //is card visible?
-        //             } else if (this.arrayOfCards[cardIndex].visible == true) {
-        //                 if (singleCard.classList.contains('card--covered')) {
-        //                     singleCard.classList.remove('card--covered')
-        //                 }
-        //                 /* singleCard.classList.remove('card--completed')//testing only*/
-        //                 singleCard.classList.add('card--visible')
-        //                 //set card covered
-        //             } else singleCard.classList.add('card--covered')
+        while (cardIndex < this.arrayOfCards.length) {
+            for (let r = 0; r < this.boardDimension; r++) {
+                let row = document.createElement('div')
+                row.classList.add('boardRow')
+                gameBoard.appendChild(row)
+                //single cards(as many as dimension) in rows
+                for (let i = 0; i < this.boardDimension; i++) {
+                    let singleCard = document.createElement('div')
+                    //is card completed?
+                    if (this.arrayOfCards[cardIndex].completed == true) {
+                        if (singleCard.classList.contains('card--visible')) {
+                            singleCard.classList.remove('card--visible')
+                        }
+                        singleCard.classList.add('card--completed')
+                        //is card visible?
+                    } else if (this.arrayOfCards[cardIndex].visible == true) {
+                        if (singleCard.classList.contains('card--covered')) {
+                            singleCard.classList.remove('card--covered')
+                        }
+                        /* singleCard.classList.remove('card--completed')//testing only*/
+                        singleCard.classList.add('card--visible')
+                        //set card covered
+                    } else singleCard.classList.add('card--covered')
 
-        //             singleCard.setAttribute('id', this.cardId)
-        //             row.appendChild(singleCard)
-        //             this.cardId++
-        //             cardIndex++
-        //         }
-        //     }
-        // }
+                    singleCard.setAttribute('id', this.cardId)
+                    row.appendChild(singleCard)
+                    this.cardId++
+                    cardIndex++
+                }
+            }
+        }
     }
 
 
