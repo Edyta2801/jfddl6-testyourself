d93f2d9a848b82cf63bb33c56db715dd57b098d4
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-08 06:55:00 +0100

    modify file

--- script/game.js
+++ script/game.js
@@ -29,13 +29,15 @@ class Card {
 
 class Game {
     constructor() {
-        this.arrayOfCards = [] //[new Card(1), new Card(2), new Card(3), new Card(4)] //array w której będą obiekty typu Card
+        this.arrayOfCards = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8] //[new Card(1), new Card(2), new Card(3), new Card(4)] //array w której będą obiekty typu Card
         //this.preDefinedArraySizes
         this.boardDimension = 4// 2,4,6,8,10 max 2x2,4x4...
         this.moveCounter = 0
         this.timer = null
         this.cardId = 1 //card/cell id for the gameboard cells/cards
-
+        this.arrayValues= [];
+        this.arrayCardIds = [];
+        this.cardFlipped = 0;
         this.init()
     }
 
@@ -106,6 +108,56 @@ class Game {
     }
 
 
+    
+    newBoardOfCards(){
+        tilesFlipped=0;
+        const output='';
+        this.arrayOfCards.shuffleDecksInArray();
+        for (let i=0;i<this.arrayOfCards.length;i++){
+            utput += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+arrayOfCards[i]+'\')"></div>';
+        }
+            document.getElementById('arrayOfCards').innerHTML=output;
+    }
+
+    memoryFlipTile(tile,val){
+        if(tile.innerHTML == "" && this.arrayValues.length < 2){
+            tile.style.background = '#FFF';
+            tile.innerHTML = val;
+            if(this.arrayValues.length == 0){
+                this.arrayValues.push(val);
+                this.arrayCardIds.push(tile.id);
+            } else if(this.arrayValues.length == 1){
+                this.arrayValues.push(val);
+                this.arrayCardIds.push(tile.id);
+                if(this.arrayValues[0] == this.arrayValues[1]){
+                    this.cardFlipped += 2;
+                    // Clear both arrays
+                    this.arrayValues = [];
+                    this.arrayCardIds = [];
+                    // Check to see if the whole board is cleared
+                    if( this.cardFlipped == this.arrayOfCards.length){
+                        alert("Board cleared... generating new board");
+                        document.getElementById('gameboard').innerHTML = "";
+                        newBoard();
+                    }
+                } else {
+                    function flip2Back(){
+                        // Flip the 2 tiles back over
+                        var tile_1 = document.getElementById(this.arrayCardIds[0]);
+                        var tile_2 = document.getElementById(this.arrayCardIds[1]);
+                        tile_1.style.background = 'url(back.png) no-repeat';
+                        tile_1.innerHTML = "";
+                        tile_2.style.background = 'url(back.png) no-repeat';
+                        tile_2.innerHTML = "";
+                        // Clear both arrays
+                        this.arrayValues = [];
+                        this.arrayCardIds = [];
+                    }
+                    setTimeout(flip2Back, 700);
+                }
+            }
+        }
+    }
 
 
 
