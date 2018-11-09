218f4d25a3a08662f185fe4dbbbd245db1d136f9
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-09 10:04:08 +0100

    modify game.js

--- script/game.js
+++ script/game.js
@@ -2,7 +2,7 @@ class Card {
     constructor(id) {
         this.visible = true
         this.completed = false
-        this.image ='' //jakiś adres do pliku z obrazkiem
+        this.image = '' //jakiś adres do pliku z obrazkiem
         this.id = null || id //przypisanie id chyba dopiero w funkcji generate array w obiekcie Game
     }
 
@@ -34,10 +34,10 @@ class Game {
         this.moveCounter = 0
         this.timer = null
         this.cardId = 1 //card/cell id for the gameboard cells/cards
-        this.activeCard=''
-        this.activeCards=[]
+        this.activeCard = ''
+        this.activeCards = []
         // this.gamePairs=arrayOfCards.length/2
-        this.gameresult=0;
+        this.gameresult = 0;
         this.init()
     }
 
@@ -50,20 +50,20 @@ class Game {
         console.log('shuffled', this.arrayOfCards)
 
         // singleCard.classList.remove('card--visible')
-    
+
     }
     // hiddenCards(){
-    
+
     // // const hidden=arrayOfCards.forEach(function(card){return card.visible===false})
     // // console.log(hidden);
-    
+
     startGame() {
 
     }
 
     render() {
         document.body.innerHTML = ""
-        
+
 
         const gameBoard = document.createElement('div')
         gameBoard.classList.add('gameboard')
@@ -74,7 +74,7 @@ class Game {
             singleCard.classList.add(card.id)
             singleCard.style.flexBasis = 100 / this.boardDimension + '%'
             // @TODO click card funcntion
-            
+
             singleCard.addEventListener('click', () => console.log(i, this.arrayOfCards[i]))
 
             if (card.completed === true) {
@@ -84,7 +84,7 @@ class Game {
             if (card.visible === true) {
                 singleCard.classList.add('card--visible')
 
-                singleCard.style.backgroundImage=`url("${card.image}")`
+                singleCard.style.backgroundImage = `url("${card.image}")`
 
 
             }
@@ -100,21 +100,21 @@ class Game {
     }
 
     clickCard() {
-   
-    this.activeCard.classList.remove("card--visible") 
-        if(this.activeCard.length===0){
-            activeCards[0]=this.activeCard
+
+        this.activeCard.classList.remove("card--visible")
+        if (this.activeCard.length === 0) {
+            activeCards[0] = this.activeCard
             console.log(1)
             return;
         }
-        else{
-            cards.forach(card=>{
+        else {
+            cards.forach(card => {
                 card.removEventListener("click", clisckard)
             })
 
         }
-   
-   
+
+
     };
 
 
@@ -123,18 +123,18 @@ class Game {
 
         activeCard = this; //w co zostało kliknięte
         //console.log(event.target) //o ile przekazane event to to samo co this
-    
+
         //czy to kliknięcie w ten sam element (tylko drugi może dać true) Musi być przed ukryciem dodane
         if (activeCard == activeCards[0]) return;
-    
+
         activeCard.classList.remove(""); //ukrycie karty, która została kliknięta
-    
+
         //czy to 1 kliknięcie, czy tablica ma długość 0
         if (activeCards.length === 0) {
             console.log("1 element");
             activeCards[0] = activeCard; //przypisanie do pozycji numer 1 wybranej karty
             return;
-    
+
         }
         //czy to 2 kliknięcie - else bo jeśli nie pierwsze, to drugie
         else {
@@ -143,7 +143,7 @@ class Game {
             cards.forEach(card => card.removeEventListener("click", clickCard))
             //ustawienie drugiego kliknięcia w tablicy w indeksie 1
             activeCards[1] = activeCard;
-    
+
             //Pół sekundy od odsłoniecia - decyzja czy dobrze czy źle
             setTimeout(function () {
                 //sprawdzenie czy to te same karty - wygrana
@@ -169,11 +169,11 @@ class Game {
                 activeCard = ""; //aktywna karta pusta
                 activeCards.length = 0; //długość tablicy na zero
                 cards.forEach(card => card.addEventListener("click", clickCard))//przywrócenie nasłuchiwania
-    
+
             }, 500)
         }
     };
-    
+
     //PART 2 - LOSOWANIE, POKAZANIE I UKRYCIE, NASŁUCHIWANIE NA KLIKA
     //Funkcja po starcie zainicjowana
     const init = function () {
@@ -228,7 +228,7 @@ class Game {
 
         for (let i = 0; i < fullDim; i++) {
             this.arrayOfCards[i] = new Card()
-            this.arrayOfCards[i].image = `./images/gameCards/${i+1}.svg`
+            this.arrayOfCards[i].image = `./images/gameCards/${i + 1}.svg`
         }
 
         const tempArr = this.arrayOfCards.map(element => Object.assign(Object.create(Card.prototype), element));
