7b108febbb2b9ca68bcf4dd1be89e82783d77fbe
Karol-Szykula <karol.szykula@wp.pl>
2018-11-10 22:50:42 +0100

    Add prices calculator logic

--- script/prices-calc.js
+++ script/prices-calc.js
@@ -1,23 +1,100 @@
-const slider = document.querySelector("#price-slider")
-const usersOutput = document.querySelector(".section-prices-calc__users-output")
-const priceOutput = document.querySelector(".section-prices-calc__price-output")
+class Price {
+    constructor() {
+        this.oneUserPrice = 5
+        this.finalPrice = 0
 
-const checkBoxCommercial = document.querySelector("#commercial")
-const checkBoxNonCommercial = document.querySelector("#non-commercial")
+        this.checkBoxCommercialValue = false
+        this.checkBoxNonCommercialValue = true
 
-usersOutput.innerHTML = slider.value
+        this.sliderValue = 5
 
-slider.oninput = function () {
-    output.innerHTML = slider.value
+        this.init()
+
+
+    }
+
+    init() {
+
+        this.slider = document.querySelector("#price-slider")
+        this.usersOutput = document.querySelector(".section-prices-calc__users-output")
+        this.priceOutput = document.querySelector(".section-prices-calc__price-output")
+
+        this.checkBoxCommercial = document.querySelector("#commercial")
+        this.checkBoxNonCommercial = document.querySelector("#non-commercial")
+
+        this.sliderRow = document.querySelector('#section-prices-calc__slider-div')
+        this.allSliderRowElements = this.sliderRow.children
+
+        this.checkBoxCommercial.onchange = this.onCommercialCheckBoxChange.bind(this)
+        this.checkBoxNonCommercial.onchange = this.onNonCommercialCheckBoxChange.bind(this)
+
+        this.slider.oninput = this.onSliderInput.bind(this)
+
+        this.countPrice()
+        this.render()
+    }
+
+    render() {
+
+        this.usersOutput.innerHTML = this.sliderValue
+
+        this.checkBoxCommercial.checked = this.checkBoxCommercialValue
+        this.checkBoxNonCommercial.checked = this.checkBoxNonCommercialValue
+
+        this.priceOutput.innerHTML = `${this.finalPrice} zł/miesiąc`
+    }
+
+    countPrice() {
+        if (this.checkBoxNonCommercialValue === true) {
+            this.finalPrice = 0
+            this.disableSliderDivElements()
+        }
+        else {
+            this.finalPrice = this.oneUserPrice * this.sliderValue
+            this.enableSliderDivElements()
+        }
+    }
+
+    onCommercialCheckBoxChange() {
+        this.checkBoxCommercialValue = true
+        this.checkBoxNonCommercialValue = false
+
+        this.countPrice()
+
+        this.render()
+    }
+
+    onNonCommercialCheckBoxChange() {
+        this.checkBoxCommercialValue = false
+        this.checkBoxNonCommercialValue = true
+
+        this.countPrice()
+
+        this.render()
+    }
+
+    onSliderInput() {
+        this.sliderValue = this.slider.value
+
+        this.countPrice()
+
+        this.render()
+    }
+
+    disableSliderDivElements() {
+
+        for (let i = 0, l = this.allSliderRowElements.length; i < l; ++i) {
+            this.allSliderRowElements[i].readOnly = true;
+            this.allSliderRowElements[i].disabled = true;
+        }
+    }
+    enableSliderDivElements() {
+
+        for (let i = 0, l = this.allSliderRowElements.length; i < l; ++i) {
+            this.allSliderRowElements[i].readOnly = false;
+            this.allSliderRowElements[i].disabled = false;
+        }
+    }
 }
 
-checkBoxCommercial.onchange = function () {
-    checkBoxCommercial.checked = true
-    checkBoxNonCommercial.checked = false
-}
-checkBoxNonCommercial.onchange = function () {
-    checkBoxCommercial.checked = false
-    checkBoxNonCommercial.checked = true
-
-    priceOutput.innerHTML = '0 zł'
-}
+const price = new Price()
