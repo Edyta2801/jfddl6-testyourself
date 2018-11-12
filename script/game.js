51d07b2443d096ba8f3089c8772ccd64c1b4aba7
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-11-12 23:32:51 +0100

    fixed yellow tile bug, styled cells

--- script/game.js
+++ script/game.js
@@ -26,9 +26,9 @@ class Card {
 
 class Game {
     constructor() {
-        this.arrayOfCards = [] //[new Card(1), new Card(2), new Card(3), new Card(4)] //array w której będą obiekty typu Card
+        this.arrayOfCards = []
         //this.preDefinedArraySizes
-        this.boardDimension = 2// 2,4,6,8,10 max 2x2,4x4...
+        this.boardDimension = 10// 2,4,6,8,10 max 2x2,4x4...
         this.moveCounter = 0
         this.timer = null
         this.cardId = 1 //card/cell id for the gameboard cells/cards
@@ -63,12 +63,12 @@ class Game {
 
             singleCard.addEventListener('click', () => {
                 this.clickCard(i)
-                // this.arrayOfCards[i].toggleVisible()
             })
-
             if (card.completed === true) {
                 singleCard.classList.add('card--completed')
+                singleCard.style.backgroundImage = `url("${card.image}")`
             }
+
             if (card.visible === true) {
                 singleCard.classList.add('card--visible')
 
@@ -107,6 +107,7 @@ class Game {
         }
 
         this.checkIfAllCompletedThenWin()
+        
     }
 
     toggleVisible(i) {
@@ -115,27 +116,25 @@ class Game {
     }
 
     checkIfVisibleCardMatchedThenCompleteThem() {
+        console.log('aaa', this.arrayOfCards)
         const uncompletedCards = this.arrayOfCards.filter((card) => !card.completed)
         const visibleCards = uncompletedCards.filter((card) => card.visible)
-
+        console.log('unc', uncompletedCards)
         if (visibleCards[0].image === visibleCards[1].image) {
             visibleCards[0].completed = true
             visibleCards[1].completed = true
         }
-
         this.render()
     }
 
     hideAllVisibleCards() {
         const uncompletedCards = this.arrayOfCards.filter((card) => !card.completed)
         const visibleCards = uncompletedCards.filter((card) => card.visible)
-
         visibleCards.map(card => card.visible = false)
     }
 
     checkIfAllCompletedThenWin() {
         const uncompletedCards = this.arrayOfCards.filter((card) => !card.completed)
-
         if (uncompletedCards.length === 0) {
             this.win()
         }
@@ -144,7 +143,7 @@ class Game {
     win() {
         console.log('YOU WON!')
     }
-
+    
     generateArrayOfCards() {
         let fullDim = (this.boardDimension * this.boardDimension) / 2
 
@@ -152,45 +151,23 @@ class Game {
             this.arrayOfCards[i] = new Card()
             this.arrayOfCards[i].image = `./images/gameCards/${i + 1}.svg`
         }
-
         const tempArr = this.arrayOfCards.map(element => Object.assign(Object.create(Card.prototype), element));
-
-
         this.arrayOfCards = this.arrayOfCards.concat(tempArr)
-
-        // for (let i = 0; i < this.arrayOfCards.length / 2; i++) {
-        //     this.arrayOfCards[i].setId('a' + i)
-        // }
-        // let k = 0
-        // for (let i = (this.arrayOfCards.length) / 2; i < this.arrayOfCards.length; i++ , k++) {
-        //     this.arrayOfCards[i].setId('b' + k)
-        // }
     }
 
 
 
     shuffleDecksInArray() {
-
         let inputArray = this.arrayOfCards.map(element => Object.assign(Object.create(Card.prototype), element));
 
         for (let i = inputArray.length - 1; i >= 0; i--) {
-
             let randomIndex = Math.floor(Math.random() * (i + 1));
             let itemAtIndex = inputArray[randomIndex];
-
             inputArray[randomIndex] = inputArray[i];
             inputArray[i] = itemAtIndex;
         }
-
         this.arrayOfCards = inputArray.map(element => Object.assign(Object.create(Card.prototype), element));
-
     }
-    attachEvent() {
-
-    }
-
-
-
 
     setGameLevel(level) {//levels 2/4/6/8/10max
         this.boardDimension = level
@@ -201,7 +178,7 @@ class Game {
     // }
 }
 
-const game1 = new Game()
-game1.render()
+const game = new Game()
+game.render()
 
 
